05fd2ec37c233313b04ca610d23e9134b2c353f4
Jakob Hermansson <jh225ud@student.lnu.se>
2025-09-11 13:38:54 +0200

    Small update with ideas

---
+++ RunningCalculator.js
@@ -0,0 +1,15 @@
+// RunningCalculator - A module for running-related calculations
+
+class RunningCalculator {
+
+    // Beräkna pace: tid/distans
+
+    // Konvertera pace till hastighet (km/h)
+
+    // Beräkna tid från distans och pace
+
+    // Beräkna distans från tid och pace 
+
+    // Beräkna pulszoner från maxpuls
+}
+
