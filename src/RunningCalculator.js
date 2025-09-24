b0e1420d42be4870d78b7bb3f6d6726fe48bcde6
Jakob Hermansson <jh225ud@student.lnu.se>
2025-09-24 14:12:55 +0200

    Added input validation in RunningCalculator

--- src/RunningCalculator.js
+++ src/RunningCalculator.js
@@ -2,35 +2,49 @@ class RunningCalculator {
 
     // Calculate pace: time/distance
     calculatePace(distance, time) {
+        this.#inputValidation(distance)
+        this.#inputValidation(time)
         return time / distance
     }
 
     // Convert pace (min/km) to speed (km/h)
     paceToSpeed(pace) {
+        this.#inputValidation(pace)
         return 60 / pace
     }
 
     // Calculate time from distance and pace
     calculateTime(distance, pace) {
+        this.#inputValidation(distance)
+        this.#inputValidation(pace)
         return distance * pace
     }
 
     // Calculate distance from time and pace 
     calculateDistance(time, pace) {
+        this.#inputValidation(time)
+        this.#inputValidation(pace)
         return time / pace
     }
 
     // Format time from decimal minutes to h, min, sec
     formatTime(decimalMinutes) {
-        const hours = Math.floor(decimalMinutes / 60);
-        const minutes = Math.floor(decimalMinutes % 60);
-        const seconds = Math.round((decimalMinutes - Math.floor(decimalMinutes)) * 60);
+        this.#inputValidation(decimalMinutes)
+        const hours = Math.floor(decimalMinutes / 60)
+        const minutes = Math.floor(decimalMinutes % 60)
+        const seconds = Math.round((decimalMinutes - Math.floor(decimalMinutes)) * 60)
         if (hours > 0) {
-            return `${hours} h ${minutes} min ${seconds} sec`;
+            return `${hours} h ${minutes} min ${seconds} sec`
         } else {
-            return `${minutes} min ${seconds} sec`;
+            return `${minutes} min ${seconds} sec`
+        }
+    }
+
+    #inputValidation(value) {
+        if (value < 0) {
+            throw new Error("Input must be a positive number.")
         }
     }
 }
 
-export default RunningCalculator;
+export default RunningCalculator
