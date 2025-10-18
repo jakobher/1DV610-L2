922ed047b0ab6a5e67c9fc6c7835903f11a0b514
Jakob Hermansson <jh225ud@student.lnu.se>
2025-10-18 11:10:28 +0200

    Add Jest tests for TrainingPlanGenerator

---
+++ __tests__/TrainingPlanGenerator.test.js
@@ -0,0 +1,23 @@
+import TrainingPlanGenerator from "../src/TrainingPlanGenerator";
+
+describe("TrainingPlanGenerator", () => {
+  test('generateWeeklyRunningPlan: 3 days should return plan with 7 days total', () => {
+    const planner = new TrainingPlanGenerator()
+    const plan = planner.generateWeeklyRunningPlan(3)
+    expect(plan).toHaveLength(7)
+  })
+
+  test('generateWeeklyRunningPlan: 3 days should have 3 workout days', () => {
+    const planner = new TrainingPlanGenerator()
+    const plan = planner.generateWeeklyRunningPlan(3)
+    const workoutDays = plan.filter(day => day.workout !== 'Rest')
+    expect(workoutDays).toHaveLength(3)
+  })
+
+  test('generateWeeklyRunningPlan: should throw error for invalid days', () => {
+    const planner = new TrainingPlanGenerator()
+    expect(() => {
+        planner.generateWeeklyRunningPlan(8)
+    }).toThrow('Running days per week must be between 1 and 7')
+    })
+  })
